8c8b5d13a6873255770da8162359b29d02851bc0
Vladimir Rybalchenko <rybalchenko.v4@gmail.com>
2024-01-23 19:14:46 +0300

    commit

--- task3/obj/Debug/net8.0/task3.AssemblyInfo.cs
+++ task3/obj/Debug/net8.0/task3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f8b252363c156717ec0f27ad2d492271f8dd582")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("task3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
