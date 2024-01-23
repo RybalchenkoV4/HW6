1f8b252363c156717ec0f27ad2d492271f8dd582
Vladimir Rybalchenko <rybalchenko.v4@gmail.com>
2024-01-23 11:42:39 +0300

    creat dir task3

---
+++ task3/Program.cs
@@ -0,0 +1,11 @@
+﻿//Задача 3: Задайте произвольную строку. Выясните, является ли она палиндромом.
+
+string str = Console.ReadLine();
+
+for (int i = 0; i < str.Length; i++)
+{
+    for (int j = 0; j < str.Length; j++)
+    {
+        
+    }
+}
