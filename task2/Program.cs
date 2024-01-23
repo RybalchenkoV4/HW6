0df8fa8205cfedae0931416a0cafde766c922860
Vladimir Rybalchenko <rybalchenko.v4@gmail.com>
2024-01-23 11:37:42 +0300

    add task2

---
+++ task2/Program.cs
@@ -0,0 +1,5 @@
+﻿//Задача 2: Задайте строку, содержащую латинские буквы в обоих регистрах. Сформируйте строку, в которой все заглавные буквы заменены на строчные.
+
+string str = "jhGjhGJYUgvhvhHJjbgvggJHGg";
+string strNew = str.ToLower();
+System.Console.WriteLine(strNew);
