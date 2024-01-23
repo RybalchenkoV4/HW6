fad090b4e3152029cdf755f5bbc7cb5be6538b8f
Vladimir Rybalchenko <rybalchenko.v4@gmail.com>
2024-01-23 23:42:57 +0300

    add task4

--- task4/Program.cs
+++ task4/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿//Задача 4*(не обязательная): Задайте строку, состоящую из слов, разделенных пробелами. Сформировать строку, в которой слова расположены в обратном порядке. В полученной строке слова должны быть также разделены пробелами.
+
+
+
+string? input = Console.ReadLine();
+
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
+string[] words = input.Split(' ');
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
+
+Array.Reverse(words);
+
+string reverseString = String.Join(" ", words);
+
+Console.WriteLine(reverseString);
